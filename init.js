84aea81b79b5fbe3fabf9750d6d8622a192d291f
mr-wang1009 <1115378579@qq.com>
2022-07-06 17:15:00 +0800

    chore: setup website

---
+++ init.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const WHITE_LIST = ['README.md', 'index.html', 'images']
+function init() {
+  let data = [], index = 0
+  const dirs = fs.readdirSync(__dirname)
+  dirs.forEach(function (dir) {
+    if (dir.charAt(0) === '.' || WHITE_LIST.includes(dir)) {
+      return
+    } else {
+      data.push({
+        id: index++,
+        name: dir,
+        Path: dir + '/index.html',
+      })
+    }
+  })
+  fs.writeFileSync('paths.json', JSON.stringify(data), 'utf8')
+}
+init()
